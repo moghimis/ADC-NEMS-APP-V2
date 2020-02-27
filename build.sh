d33f06ad27b731ab71ed2a3505e05fd6cd9df9f2
Saeed Moghimi <Saeed.Moghimi@noaa.gov>
2020-02-27 21:05:26 +0000

    initiate moving to new NEMS with helps and supports from beheen.m.trimble@noaa.gov

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# moghimis@gmail.com 
+# Script to compile NSEModel NEMS application
+
+## Feb 27, 2020
+
+# load modules
+source modulefiles/hera/coastal_app
+
+cd NEMS
+
+#clean up
+make -f GNUmakefile distclean_ADCIRC COMPONENTS="ADCIRC"
+make -f GNUmakefile distclean_WW3DATA COMPONENTS="WW3DATA"
+make -f GNUmakefile distclean_ATMESH COMPONENTS="ATMESH"
+
+#make
+make -f GNUmakefile build COMPONENTS="ADCIRC WW3DATA ATMESH"
+
+
+
